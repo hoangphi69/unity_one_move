fff7a4672d7db8e108ed490b0d096c8b9a4d1c87
Hoang Phi <69hoangphi69@gmail.com>
2026-03-14 12:52:22 +0700

    test

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-  [SerializeField] private int levelPoint = 1;
   [SerializeField] private string cutsceneName;
   [SerializeField] private SceneField scene;
 
